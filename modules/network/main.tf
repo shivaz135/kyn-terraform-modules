f8caabc6de92fd78ee6e89ac8dd5aaf56ddb8fbd
siva <shivazmailbox@gmail.com>
2026-09-25 09:32:26 +0000

    network module code added

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -20,6 +20,6 @@ resource "azurerm_virtual_network" "this" {
 resource "azurerm_subnet" "web" {
   name                 = "${var.project_name}-${var.environment}-${var.resource_group_name}-web-subnet"
   resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.id
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = var.web_subnet_address
 }
